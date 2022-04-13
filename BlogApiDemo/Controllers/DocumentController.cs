30a263623f65eca82193a7925ddb89488a8b0265
Mashite <masite_bayram@hotmail.com>
2022-04-14 01:28:36 +0300

    ok

---
+++ BlogApiDemo/Controllers/DocumentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApiDemo.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DocumentController : ControllerBase
+	{
+		[HttpGet]
+		[Route("CreatePdf")]
+		public ActionResult<string> CreatePdf(string Html)
+		{
+			
+
+				return Html.Substring(0,1); // return PDF as base64 string
+			
+		}
+	}
+}
